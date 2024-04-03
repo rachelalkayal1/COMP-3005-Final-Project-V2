49f3156a5331176a43dcec77f2e294defe0cb3ce
youssefbedeirhussein <youssefbedeirhussein@cmail.carleton.ca>
2024-04-03 02:15:53 -0400

    done

--- src/databaseConnection.js
+++ src/databaseConnection.js
@@ -1,1 +1,11 @@
+const { Client } = require("pg");
 
+const client = new Client({
+  host: "localhost",
+  user: "postgres",
+  port: 5432,
+  password: "Ybh25765",
+  database: "finalProject",
+});
+
+module.exports = client; 
