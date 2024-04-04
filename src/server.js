e57e7c92969444cdbf35c7dcec07978dc9271ffb
youssefbedeirhussein <youssefbedeirhussein@cmail.carleton.ca>
2024-04-04 15:41:44 -0400

    done

--- src/server.js
+++ src/server.js
@@ -1,1 +1,104 @@
 
+//connect to the database on the server side 
+const db = require ('./databaseConnection.js');
+const express = require ('express'); 
+const pug = require('pug');
+const app = express(); 
+const port = 3000;
+
+app.listen(port, ()=>{
+    console.log("Sever is now listening at port 3000");
+});
+
+app.set('views', './views'); 
+app.set('view engine', 'pug');
+
+app.use(express.json());
+
+db.connect();
+
+app.get("/", renderLogin); 
+app.get("/newMember", renderSignUp); 
+app.get("/healthInformation", redirectHealthInformation);
+app.get("/exerciseRoutine", renderExercisePage); 
+app.get("/fitnessAchievements", renderFitnessAchievements);
+app.get("/allClasses", renderClassPage); 
+app.get("/bookASession", renderPrivateSession);
+app.post("/addMember", addNewMember); 
+app.post("/:username", validateLogin);
+app.get("/:username", redirectDashboard);
+
+
+let username; 
+let password; 
+//show the login page
+function renderLogin (req, res){
+    res.render('login');
+    db.end;
+}
+
+//show the sign up page for new members
+function renderSignUp (req, res){
+    res.render('signupPage');
+    db.end;
+}
+
+function validateLogin(req, res){
+    username = req.body.username; 
+    password = req.body.password; 
+
+    let result = db.query(`SELECT * FROM members WHERE username = '${username}' and userpassword = '${password}'`);
+
+    if(result){
+        res.status(200); 
+        res.end();
+    }
+}
+
+function redirectDashboard (req, res){
+    res.render('dashboard', {user: username});
+}
+
+async function addNewMember (req, res){
+    let firstName = req.body.firstName;
+    let lastName = req.body.lastName; 
+    //let username = req.body.username; 
+    //let password = req.body.password;
+    let height = req.body.height; 
+    let weight = req.body.weight; 
+    let dob = req.body.dob;
+    let medication = req.body.medication
+    
+    await db.query(`INSERT INTO members (username, userPassword) VALUES ('${username}', '${password}')`);
+
+    const memberIDResult = await db.query(`SELECT memberID FROM members WHERE username = '${username}'`);
+    const memberID = memberIDResult.rows[0].memberid;
+
+    await db.query(`INSERT INTO medicationList (nameOfMedication, memberID) VALUES ('${medication}', ${memberID})`);
+
+    const medicationIDResult = await db.query(`SELECT medicationID FROM medicationList WHERE memberID = ${memberID}`);
+    const medicationID = medicationIDResult.rows[0].medicationid;
+
+    res.status(200).send({ memberID, medicationID });
+    res.end();
+}
+   
+function redirectHealthInformation (req, res) {
+    res.render('healthInformation');
+}
+
+function renderExercisePage (req, res){
+    res.render('exerciseRoutine'); 
+}
+
+function renderFitnessAchievements (req, res) {
+    res.render('fitnessAchievements');
+}
+
+function renderClassPage (req, res) {
+    res.render('classes'); 
+}
+
+function renderPrivateSession(req, res) {
+    res.render('bookASession');
+}
---
+++ src/views/login.js
@@ -0,0 +1,128 @@
+function loginInfo(){
+
+    //get the username and the password of the existing member 
+    let username = document.getElementById("username").value; 
+    let password = document.getElementById("password").value; 
+
+    //handle empty inputs
+    if(!username){
+        document.getElementById('userError').innerHTML = `<p id="userError">Please enter your username`; 
+    }else{
+        document.getElementById('userError').innerHTML = " ";
+    }
+
+    if(!password){
+        document.getElementById('passError').innerHTML = `<p id="userError">Please enter your password`; 
+    }else{
+        document.getElementById('passError').innerHTML = " ";
+    }
+
+    if(username && password){
+        let loginInformation = {username : username, password : password}; 
+        let loginRequest = new XMLHttpRequest(); 
+
+        loginRequest.open("POST", "/" + username); 
+        loginRequest.setRequestHeader('Content-Type', 'application/JSON');
+
+        loginRequest.onreadystatechange = function(){
+            if(this.readyState == 4 && this.status == 200){
+                //redirect user to the dashboard once they are logged in
+                window.location = "http://localhost:3000/" + username;
+            }
+        }
+        loginRequest.send(JSON.stringify(loginInformation));
+    }
+
+}
+
+function signupPage() {
+    let signupRequest = new XMLHttpRequest(); 
+
+    signupRequest.open("GET", "/newMember"); 
+    signupRequest.setRequestHeader('Content-Type', 'application/JSON');
+
+    signupRequest.onreadystatechange = function(){
+        if(this.readyState == 4 && this.status == 200){
+            window.location = "http://localhost:3000/newMember/";
+        }
+    }   
+
+    signupRequest.send();
+}
+
+function create(){
+    let firstName = document.getElementById("firstName").value;
+    let lastName = document.getElementById("lastName").value;
+    let dob = document.getElementById('dob').value;
+    let username = document.getElementById('username').value;
+    let password = document.getElementById('password').value;
+    let height = document.getElementById('height').value;
+    let weight = document.getElementById('weight').value;
+    let medication = document.getElementById('medicine').value;
+
+    if(!firstName){
+        document.getElementById('firstNameError').innerHTML = `<p id="firstNameError">Please enter your first name`; 
+    }else{
+        document.getElementById('firstNameError').innerHTML = " ";
+    }
+
+    if(!lastName){
+        document.getElementById('lastNameError').innerHTML = `<p id="lastNameError">Please enter your last name`; 
+    }else{
+        document.getElementById('lastNameError').innerHTML = " ";
+    }
+
+    if(!username){
+        document.getElementById('usernameError').innerHTML = `<p id="usernameError">Please enter a Username`; 
+    }else{
+        document.getElementById('usernameError').innerHTML = " ";
+    }
+
+    if(!dob){
+        document.getElementById('dateError').innerHTML = `<p id="dateError">Please enter your date of birth`; 
+    }else{
+        document.getElementById('dateError').innerHTML = " ";
+    }
+    
+    if(!password){
+        document.getElementById('passError').innerHTML = `<p id="passError">Please enter a password`; 
+    }else{
+        document.getElementById('passError').innerHTML = " ";
+    }
+
+    if(!height){
+        document.getElementById('heightError').innerHTML = `<p id="heightError">Please enter your height`; 
+    }else{
+        document.getElementById('heightError').innerHTML = " ";
+    }
+
+    if(!weight){
+        document.getElementById('weightError').innerHTML = `<p id="weightError">Please enter your weight`; 
+    }else{
+        document.getElementById('weightError').innerHTML = " ";
+    }
+
+    if(!medication){
+        document.getElementById('medicineError').innerHTML = `<p id="medicineError">Please enter your medication or N/A`; 
+    }else{
+        document.getElementById('medicineError').innerHTML = " ";
+    }
+
+    let userInformation = {firstName : firstName, lastName : lastName, dob : dob, username : username, password : password, height : height, weight : weight, medication : medication};
+    let addMemberRequest = new XMLHttpRequest();
+    addMemberRequest.open("POST", "/addMember", true);
+    addMemberRequest.setRequestHeader("Content-Type", "application/json");
+
+    addMemberRequest.onreadystatechange = function() {
+        if(this.readyState == 4 && this.status == 200){
+            //redirect user to the dashboard once they are logged in
+            window.location = "http://localhost:3000/" + username;
+            let response = this.responseText; 
+            console.log(response);
+        }
+    }
+    addMemberRequest.send(JSON.stringify(userInformation));
+
+}
+    
+
