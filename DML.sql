a338824e8768ddddf9f32d8852c4614e8ff686f4
rachelalkayal <rachelalkayal@cmail.carleton.ca>
2024-04-13 16:51:40 -0400

    finished code

--- DML.sql
+++ DML.sql
@@ -31,7 +31,7 @@ VALUES
     ('N/A', 4); 
 
 -- Sample data for memberInfo table
-INSERT INTO memberInfo (firstName, lastName, dateOfBirth, memberWeight, memberHeight, trainer, adminStaff, medicationID, memberID)
+INSERT INTO memberInfo (firstName, lastName, dateOfBirth, memberWeight, memberHeight, medicationID, memberID)
 VALUES 
     ('Sherif', 'Bedeir Hussein', '1974-01-20', 70.5, 180, 1, 1),
     ('Moufid', 'Al Kayal', '1964-03-05', 65.2, 165, 2, 2),
@@ -39,18 +39,19 @@ VALUES
     ('Jack', 'Black', '1988-03-10', 80.3, 175, 4, 4);
 
 
-INSERT INTO trainerInfo (firstName, lastName, dateOfBirth, memberID)
+INSERT INTO trainerInfo (firstName, lastName, dateOfBirth, sessionFee, trainerID)
 VALUES
-    ('Mohammad', 'Ali', '1942-01-17', 1),
-    ('Mike', 'Tyson', '1966-06-30', 2), 
-    ('Noel', 'Job', '2003-09-05', 3), 
-    ('Joe', 'Frazier', '1944-01-12', 4);
+    ('Mohammad', 'Ali', '1942-01-17', 45, 1),
+    ('Mike', 'Tyson', '1966-06-30', 60, 2), 
+    ('Noel', 'Job', '2003-09-05', 20, 3), 
+    ('Joe', 'Frazier', '1944-01-12', 30, 4);
 
 
 INSERT INTO adminInfo (firstName, lastName, dateOfBirth, staffID)
+VALUES
     ('Rachel', 'Al Kayal', '2001-01-23', 1), 
     ('Youssef', 'Bedeir Hussein', '2003-01-11', 2), 
-    ('Lucy', 'Chen', '1978-04-31', 3); 
+    ('Lucy', 'Chen', '1978-04-30', 3), 
     ('Paul', 'Bedier', '2004-12-12', 4); 
 
 -- Sample data for memberGoals table
@@ -78,12 +79,18 @@ VALUES
     ('2024-03-01', 'Pull-ups', 290, 270, 4);
 
 -- Sample data for rooms table
-INSERT INTO rooms (roomType, dateBooked, startTime, endTime)
-VALUES 
-    ('Gym Room', '2024-03-01', '08:00:00', '10:00:00'),
-    ('Studio Room', '2024-03-01', '09:00:00', '11:00:00'),
-    ('Pool Room', '2024-03-01', '10:00:00', '12:00:00'),
-    ('Yoga Room', '2024-03-01', '11:00:00', '13:00:00');
+INSERT INTO rooms (roomType)
+VALUES  
+    ('Group Exercise Studio'), 
+    ('Spin Studio'), 
+    ('Yoga Studio'), 
+    ('Cardio Room'), 
+    ('Weightlighting Area'),
+    ('Functional Training Area'), 
+    ('Meditation Room'), 
+    ('Boxing/Martial Arts'), 
+    ('Aquatic Center'), 
+    ('Multipurpose Room'); 
 
 -- Sample data for fitnessEquipment table
 INSERT INTO fitnessEquipment (nameOfMachine, lastCheckUp, nextCheckUp, notableIssues)
@@ -94,12 +101,12 @@ VALUES
     ('Treadmill', '2024-04-01', '2024-10-01', 'Working fine');
 
 -- Sample data for class table
-INSERT INTO class (className, description, dateOfClass, startTime, endTime, duration, trainerID)
+INSERT INTO class (className, description, dateOfClass, startTime, endTime, duration, trainerID, roomID)
 VALUES 
-    ('Yoga', 'Relaxing yoga session', '2024-03-01', '10:00:00', '11:00:00', 60, 1),
-    ('Zumba', 'High-energy dance workout', '2024-03-01', '11:00:00', '12:00:00', 60, 2),
-    ('Pilates', 'Core-strengthening exercises', '2024-03-01', '14:00:00', '15:00:00', 60, 3),
-    ('Spin', 'Indoor cycling session', '2024-03-01', '15:00:00', '16:00:00', 60, 4);
+    ('Yoga', 'Relaxing yoga session', '2024-03-01', '10:00:00', '11:00:00', 60, 1, 3),
+    ('Zumba', 'High-energy dance workout', '2024-03-01', '11:00:00', '12:00:00', 60, 2, 1),
+    ('Pilates', 'Core-strengthening exercises', '2024-03-01', '14:00:00', '15:00:00', 60, 3, 1),
+    ('Spin', 'Indoor cycling session', '2024-03-01', '15:00:00', '16:00:00', 60, 4, 2);
 
 -- Sample data for membersEnrolled table
 INSERT INTO classMembers (memberFirstName, memberLastName, classID, memberID)
@@ -112,16 +119,29 @@ VALUES
 -- Sample data for privateSession table
 INSERT INTO privateSession (sessionDate, sessionTime, duration, trainerID, memberID)
 VALUES 
-    ('2024-03-01', '13:00:00', 60, 1, 1),
-    ('2024-03-01', '14:00:00', 60, 2, 2),
-    ('2024-03-01', '15:00:00', 60, 3, 3),
-    ('2024-03-01', '16:00:00', 60, 4, 4);
+    ('2024-03-01', '13:00:00', 120, 1, 1),
+    ('2024-03-01', '14:00:00', 120, 2, 2),
+    ('2024-03-01', '15:00:00', 120, 3, 3),
+    ('2024-03-01', '16:00:00', 120, 4, 4);
 
 -- Sample data for trainerSchedule table
 INSERT INTO trainerSchedule (dateOfAvailability, startTime, endTime, trainerID)
 VALUES 
-    ('2024-03-01', '08:00:00', '12:00:00', 1),
-    ('2024-03-01', '09:00:00', '13:00:00', 2),
-    ('2024-03-01', '10:00:00', '14:00:00', 3),
-    ('2024-03-01', '11:00:00', '15:00:00', 4);
+    ('2024-03-01', '08:00:00', '9:00:00', 1),
+    ('2024-03-01', '09:00:00', '10:00:00', 2),
+    ('2024-03-01', '10:00:00', '11:00:00', 3),
+    ('2024-03-01', '11:00:00', '12:00:00', 4);
 
+INSERT INTO paymentProcess (memberFirstName, memberLastName, paymentAmount, billingDate, memberID)
+VALUES 
+    ('Sherif', 'Bedeir Hussein', 100.0, '2024-04-01', 1),
+    ('Moufid', 'Al Kayal', 80.0, '2024-04-03', 2),
+    ('Julia', 'Johnson', 120.0, '2024-04-05', 3),
+    ('Jack', 'Black', 150.0, '2024-04-07', 4);
+
+INSERT INTO sessionPayment (memberFirstName, memberLastName, paymentAmount, paymentDue, paid, memberID)
+VALUES 
+    ('Sherif', 'Bedeir Hussein', 90, '2024-04-28', TRUE, 1),
+    ('Moufid', 'Al Kayal', 120, '2024-04-28', FALSE, 2),
+    ('Julia', 'Johnson', 40, '2024-04-28', FALSE, 3),
+    ('Jack', 'Black', 60, '2024-04-28', TRUE, 4);
